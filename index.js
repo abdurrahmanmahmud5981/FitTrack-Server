2929eb99b6b9440684a5c09b7e6606a8d4ba5de5
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-05-27 13:33:55 +0600

    Update index.js

--- index.js
+++ index.js
@@ -646,7 +646,7 @@ async function run() {
 
         // update user info in db
 
-        await client.db('admin').command({ ping: 1 })
+        //await client.db('admin').command({ ping: 1 })
         // console.log(
         //     'Pinged your deployment. You successfully connected to MongoDB!'
         // )
